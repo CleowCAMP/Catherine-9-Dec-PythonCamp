3727d071bdfa1b8622fbdd76cc1016cd52669abc
CleowCAMP <fatsheep.llc@gmail.com>
2025-12-09 07:16:45 +0000

    Dec 9, 2025, 7:16 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -8,10 +8,10 @@
 
 ########################################################################
 # Task 2:
-# print ("Catherine Leow")
-# print ("I am 10 yrs old")
-# print("Henry Park Primary School")
-# print("Nothing")
+print ("Catherine Leow")
+print ("I am 10 yrs old")
+print("Henry Park Primary School")
+print("Nothing")
 
 
 ########################################################################
@@ -44,6 +44,15 @@
 
 ########################################################################
 # Task 6:
+def selfIntro():
+    print ("Catherine Leow")
+print ("I am 10 yrs old")
+print("Henry Park Primary School")
+print("Nothing")
+
+
+
+
 
 
 
