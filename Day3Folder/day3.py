dd899f77c912b1dc5f977a2346471234e483eab3
CleowCAMP <fatsheep.llc@gmail.com>
2025-12-11 06:34:37 +0000

    Dec 11, 2025, 6:34 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -5,7 +5,7 @@ print("hello from day3")
 ########################################################################
 # Task 1: 
 myTitle = input ("What is your title? ")
-myName = 
+myName = input()
 print(myTitle + myName + myCommand)
 
 
