dd12a18aff0dbc69be73ae66c2be5d590b5f168c
CleowCAMP <fatsheep.llc@gmail.com>
2025-12-11 06:49:46 +0000

    Dec 11, 2025, 6:49 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -15,7 +15,7 @@ print("hello from day3")
 # Task 2:
 mynumber1 = input ("Enter number 1 ")
 mynumber2 = input ("Enter number 2 ")
-mynumber3 = 
+mynumber3 = input ("Enter n")
 
 ########################################################################
 # Task 3:
