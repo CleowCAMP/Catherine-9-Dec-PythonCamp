0dab9022f812976aaf139d8ee3d04f445b95744f
CleowCAMP <fatsheep.llc@gmail.com>
2025-12-11 06:57:05 +0000

    Dec 11, 2025, 6:57 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -16,7 +16,7 @@ print("hello from day3")
 num1 = input ("Enter number 1: ")
 num2 = input ("Enter number 2: ")
 answer = int(num1) + int(num2)
-print(num1 + " + "+ num2 + " = " +str )
+print(num1 + " + "+ num2 + " = " +str(answer)) 
 
 ########################################################################
 # Task 3:
